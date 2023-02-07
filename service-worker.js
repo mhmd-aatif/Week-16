dc12541c5f388816af0545e7817ce9e8801d0bb4
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-07 10:23:47 +0400

    service worker edits

--- service-worker.js
+++ service-worker.js
@@ -15,3 +15,36 @@ self.addEventListener("install", (e) => {
     })
   );
 });
+
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(
+//     catches.match(e.request).then(function (r) {
+//       console.log("[Service Worker] Fetching Resources");
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function (r) {
+//       console.log("[Service Worker] Fetching resource: " + e.request.url);
+//       return r;
+//     })
+//   );
+// });
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
