10c5baf8c816f47df163873ae030c3078a700fc5
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-07 10:03:43 +0400

    first commit

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+var cacheName = "petstore-v1";
+var cacheFiles = [
+  "index.html",
+  "products.js",
+  "petstore.webmanifest",
+  "home.png",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
